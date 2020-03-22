2fde652ab81a039de567ba6506f83bb2acb670a3
mpraski <marcin.praski@live.com>
2020-03-22 21:01:26 +0100

    Port fix

--- config/releases.exs
+++ config/releases.exs
@@ -25,7 +25,7 @@ secret_key_base =
 
 config :api, Api.Endpoint,
   http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
+    port: String.to_integer(System.get_env("PORT") || "8080"),
     transport_options: [socket_opts: [:inet6]]
   ],
   secret_key_base: secret_key_base
